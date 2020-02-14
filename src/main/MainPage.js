b1d33c6fdceec54a96f5f0d1ca37f949093eb4e3
piotrkedra <eloelo153>
2020-02-14 13:33:45 +0100

    #1 Mock design add basic animation for scrolling

--- src/main/MainPage.js
+++ src/main/MainPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet, TouchableOpacity, ImageBackground, StatusBar,Image} from 'react-native';
+import {View, StyleSheet, TouchableOpacity, ImageBackground, StatusBar,Image, Text, ScrollView} from 'react-native';
 
 
 export default class MainPage extends React.Component {
@@ -7,6 +7,18 @@ export default class MainPage extends React.Component {
     state = {
         scroll: false,
         days: [{date: 22, short: 'wen'},{date: 23, short: 'thr'},{date: 24, short: 'fri'},{date: 25, short: 'sun'},{date: 26, short: 'sat'},{date: 27, short: 'mon'},{date: 28, short: 'thu'}]
+    };
+
+    onScrollNotTopMinimizeHeader = (event) => {
+        console.log(event);
+
+        const y = event.nativeEvent.contentOffset.y;
+        if(y > 0 && this.state.scroll===false){
+            this.setState({scroll: true});
+        }
+        if(y === 0 && this.state.scroll===true){
+            this.setState({scroll: false});
+        }
     };
 
     render = () => {
@@ -22,21 +34,44 @@ export default class MainPage extends React.Component {
                     }}
                     source={require('../../assets/images/background.jpg')}
                 >
-                <View style={styles.upView}>
-                    <View style={!this.state.scroll ? styles.header : styles.headerOnScroll}>
-                        <TouchableOpacity style={!this.state.scroll ? styles.burgerMenu : styles.burgerMenuOnScroll} onPress={() => this.setState({scroll: !this.state.scroll})}>
-                            <Image style={{height: 33, width: 33}} source={require('../../assets/images/menu.png')}/>
-                        </TouchableOpacity>
-                        <View style={styles.leftSmallMenu}>
-                            <TouchableOpacity>
-                                <Image style={{height: 38, width: 38, marginRight: 10}} source={require('../../assets/images/global-search.png')}/>
-                            </TouchableOpacity>
-                            <TouchableOpacity>
-                                <Image style={{height: 38, width: 38, marginRight: 10}} source={require('../../assets/images/pin.png')}/>
-                            </TouchableOpacity>
+                    <View style={!this.state.scroll ?  styles.headerBar : styles.headerBarOnScroll}>
+                        <View style={styles.upView}>
+                            <View style={!this.state.scroll ? styles.header : styles.headerOnScroll}>
+                                <TouchableOpacity style={!this.state.scroll ? styles.burgerMenu : styles.burgerMenuOnScroll} onPress={() => this.setState({scroll: !this.state.scroll})}>
+                                    <Image style={{height: 33, width: 33}} source={require('../../assets/images/menu.png')}/>
+                                </TouchableOpacity>
+                                <View style={styles.leftSmallMenu}>
+                                    <TouchableOpacity>
+                                        <Image style={{height: 38, width: 38, marginRight: 10}} source={require('../../assets/images/global-search.png')}/>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity>
+                                        <Image style={{height: 38, width: 38, marginRight: 10}} source={require('../../assets/images/pin.png')}/>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        </View>
+                        <View style={{flex: 1, flexDirection: 'row', padding: 15}}>
+                            <View style={{width: '60%'}}>
+                                <Text style={{fontSize: 20}}>Widokowa 22</Text>
+                                <Text style={{fontSize: 40}}>Zabierzów</Text>
+                            </View>
+                            <View style={{width: '40%', alignItems: 'center', justifyContent: 'flex-end'}}>
+                                <Image style={{height: 60, width: 60}}
+                                       source={require('../../assets/images/snowin.png')}/>
+                            </View>
                         </View>
                     </View>
-                </View>
+                    <ScrollView style={{width: '100%'}} onScroll={this.onScrollNotTopMinimizeHeader}>
+                        <View style={{margin: '5%', marginTop: 0, width: '90%', height: 300, backgroundColor: 'white', borderRadius: 20}}>
+
+                        </View>
+                        <View style={{margin: '5%', marginTop: 0,width: '90%', height: 300, backgroundColor: 'white', borderRadius: 20}}>
+
+                        </View>
+                        <View style={{margin: '5%', marginTop: 0,width: '90%', height: 300, backgroundColor: 'white', borderRadius: 20}}>
+
+                        </View>
+                    </ScrollView>
                 </ImageBackground>
             </View>
         )
@@ -45,6 +80,12 @@ export default class MainPage extends React.Component {
 
 
 const styles = StyleSheet.create({
+    headerBar: {
+        height: '28%',
+    },
+    headerBarOnScroll:{
+        height: '11%',
+    },
     upView: {
         flexDirection: 'column',
     },
