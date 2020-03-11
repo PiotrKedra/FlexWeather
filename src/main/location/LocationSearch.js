9fb8b46f4ccc7f6a6c4242eb15de92ad7f112e70
piotrkedra <piotr.k9750@gmail.com>
2020-03-11 18:27:51 +0100

    #13 google places search autocomplete

--- src/main/location/LocationSearch.js
+++ src/main/location/LocationSearch.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import {View} from 'react-native';
-import MapView from "react-native-maps";
+import MapView from 'react-native-maps';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 
 import FooterMenu from "../menu/FooterMenu";
 
 class LocationSearch extends React.Component {
     render() {
         return (
-            <View style={{justifyContent: 'center', alignItems: 'center'}}>
-                <View style={{marginTop: 10, width: '90%', height: 300, backgroundColor: 'white', borderRadius: 20, overflow: 'hidden'}}>
+            <View style={{justifyContent: 'center', alignItems: 'center', flex: 1, backgroundColor: '#FFAD94'}}>
+                <GooglePlacesAutocomplete
+                    placeholder='Search'
+                    minLength={2}
+                    autoFocus={true}
+                    returnKeyType={'search'}
+                    listViewDisplayed={false}
+                    fetchDetails={true}
+                    query={{
+                        key: 'YOUR_API_KEY',
+                        language: 'en'
+                    }}
+                    nearbyPlacesAPI='GooglePlacesSearch'
+                    debounce={200}
+                />
+                <View style={{marginTop: 10, width: '95%', height: 300, backgroundColor: 'white', borderRadius: 10, overflow: 'hidden'}}>
                     <MapView
                         style={{
                             borderRadius: 8,
