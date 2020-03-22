abde9bbab36bdc8bdc2e8751e76a6b322885be25
piotrkedra <piotr.k9750@gmail.com>
2020-03-22 21:51:20 +0100

    ESLint problems...

--- src/main/components/CustomText.js
+++ src/main/components/CustomText.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Text} from 'react-native';
-
+import { connect } from 'react-redux';
+import { Text } from 'react-native';
 
 class CustomText extends React.PureComponent {
-
-    render() {
-        let style;
-        if (this.props.fontLoaded === true) {
-            style = [{fontFamily: 'Neucha'}, this.props.style || {}];
-        }
-        else {
-            style = this.props.style;
-        }
-        return <Text x={this.props.x} style={style}>{this.props.children}</Text>
+  render() {
+    let style;
+    if (this.props.fontLoaded === true) {
+      style = [{ fontFamily: 'Neucha' }, this.props.style || {}];
+    } else {
+      style = this.props.style;
     }
+    return (
+      <Text x={this.props.x} style={style}>
+        {this.props.children}
+      </Text>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return { fontLoaded: state.fontLoaded };
+const mapStateToProps = state => {
+  return { fontLoaded: state.fontLoaded };
 };
 
 export default connect(mapStateToProps)(CustomText);
