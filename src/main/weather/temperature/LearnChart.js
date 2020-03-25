10f858b632fbdf32142566fd4e63ac1e29c40a2a
piotrkedra <piotr.k9750@gmail.com>
2020-03-25 16:08:46 +0100

    #12 refactor v1

--- src/main/weather/temperature/LearnChart.js
+++ src/main/weather/temperature/LearnChart.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Svg, G, Line, Circle, Text, Image, Polygon, Defs, LinearGradient, Stop} from 'react-native-svg';
 import * as d3 from 'd3';
-import {connect} from 'react-redux';
 
 const svgWidth = 800;
 const svgHeight = 240;
@@ -62,9 +61,6 @@ class LearnChart extends React.PureComponent {
       polygonPoints += Math.ceil(x(item.label)) + ',' + -Math.ceil(y(item.temp)) + ' ';
     }
     polygonPoints +=  Math.ceil(x(data[data.length - 1].label)) + ',' + -startYPointOfGraph +  ' '  + Math.ceil(x(data[0].label)) + ',' + -startYPointOfGraph;
-
-    console.log(polygonPoints);
-
     return (
         <Polygon
             points={polygonPoints}
@@ -75,17 +71,17 @@ class LearnChart extends React.PureComponent {
 
   render() {
     const tmpData = [
-      {label: '8:00', temp: 5},
-      {label: '9:00', temp: 5},
-      {label: '10:00', temp: 6},
-      {label: '11:00', temp: 8},
-      {label: '12:00', temp: 9},
-      {label: '13:00', temp: 9},
-      {label: '14:00', temp: 10},
-      {label: '15:00', temp: 8},
-      {label: '16:00', temp: 7},
-      {label: '17:00', temp: 6},
-      {label: '18:00', temp: 4},
+      {label: '8:00', temp: 5, rain: '1%'},
+      {label: '9:00', temp: 5, rain: '0%'},
+      {label: '10:00', temp: 6, rain: '2%'},
+      {label: '11:00', temp: 8, rain: '2%'},
+      {label: '12:00', temp: 9, rain: '3%'},
+      {label: '13:00', temp: 9, rain: '0%'},
+      {label: '14:00', temp: 10, rain: '11%'},
+      {label: '15:00', temp: 8, rain: '20%'},
+      {label: '16:00', temp: 7, rain: '69%'},
+      {label: '17:00', temp: 6, rain: '0%'},
+      {label: '18:00', temp: 4, rain: '0%'},
     ];
     const colors = {
       axis: '#515151',
@@ -100,151 +96,167 @@ class LearnChart extends React.PureComponent {
     return (
       <Svg width={svgWidth} height={svgHeight}>
         <G y={svgHeight}>
-          {this.generateGradientComponent(tmpData, x, y)}
+          {this.getDefinitions()}
 
+          {/* grid lines */}
+          {this.generateFullLengthLine(-startYPointOfGraph - graphHeight)}
+          {this.generateFullLengthLine(-startYPointOfGraph - graphHeight / 2, 50)}
+          {this.generateFullLengthLine(-startYPointOfGraph)}
+          {this.generateVerticalGridLines(tmpData, x)}
+
+          {/* forecast images*/}
+          {this.generateForecastImageForEach(tmpData, x)}
+          {this.generateRainfallImageForEach(tmpData, x)}
+
+          {/* temperature path */}
+          {this.generateGradientComponent(tmpData, x, y)}
           {this.generateLineComponents(tmpData, x, y)}
-          {tmpData.map(item => (
-            <Text
-              key={'t' + item.label}
-              fontSize="16"
+          {this.generateDotForEach(tmpData, x, y)}
+
+          {/* min & max values*/}
+          {this.generateSingleText('MAX ' + maxValue + DEGREE_SIGN, 10, -startYPointOfGraph - graphHeight - 2)}
+          {this.generateSingleText('MIN ' + minValue + DEGREE_SIGN, 10, -startYPointOfGraph - 2)}
+
+          {/* data values ( text for: hour, temp, rainfall % ) */}
+          {this.generateDegreeTextForEachItem(tmpData, x, y)}
+          {this.generateTextForEachItem(tmpData, 'rain', x, 8, -20, 14)}
+          {this.generateTextForEachItem(tmpData, 'label', x, 0, (startYPointOfGraph + graphHeight) * -1 - 70, 20)}
+        </G>
+      </Svg>
+    );
+  }
+
+  getDefinitions() {
+    return <Defs>
+      <LinearGradient id="grad" x1="0" y1="1" x2="0" y2="0">
+        <Stop offset="1" stopColor='#5BC3CE' stopOpacity="0.3"/>
+        <Stop offset="0" stopColor="#FFF" stopOpacity="0"/>
+      </LinearGradient>
+    </Defs>;
+  }
+
+  generateForecastImageForEach(tmpData, x) {
+    return <>
+      {tmpData.map(item => (
+          <Image
+              x={x(item.label) - 17}
+              y={(startYPointOfGraph + graphHeight) * -1 - 60}
+              width={35}
+              height={35}
+              preserveAspectRatio="xMidYMid slice"
+              opacity="0.6"
+              href={require('../../../../assets/images/sun-cloud.png')}
+          />
+      ))}
+    </>;
+  }
+
+  generateDotForEach(tmpData, x, y) {
+    return <>
+      {tmpData.map(item => (
+          <Circle
+              key={'dot' + item.label}
+              cx={x(item.label)}
+              cy={y(item.temp) * -1}
+              r="2"
+              fill="#5BC3CE"
+          />
+      ))}
+    </>;
+  }
+
+  generateRainfallImageForEach(tmpData, x) {
+    return <>
+      {tmpData.map(item => (
+          <Image
+              x={x(item.label) - 20}
+              y={-35}
+              width={17}
+              height={17}
+              preserveAspectRatio="xMidYMid slice"
+              opacity="0.4"
+              href={require('../../../../assets/images/rain-umbrella.png')}
+          />
+      ))}
+    </>;
+  }
+
+  generateTextForEachItem(tmpData, itemKey, xFunction, xShift, y, fontSize) {
+    return <>
+      {tmpData.map(item => (
+          <Text
+              key={itemKey + item.label}
+              fontSize={fontSize}
+              x={xFunction(item.label) + xShift}
+              y={y}
+              textAnchor="middle"
+              fill="black"
+              fontFamily="Neucha-Regular">
+            {item[itemKey]}
+          </Text>
+      ))}
+    </>;
+  }
+
+  generateDegreeTextForEachItem(data, x, y) {
+    return <>
+      {data.map(item => (
+          <Text
+              key={'degree' + item.label}
+              fontSize={16}
               x={x(item.label)}
               y={y(item.temp) * -1 - 6}
               textAnchor="middle"
               fill="black"
               fontFamily="Neucha-Regular">
-              {item.temp + DEGREE_SIGN}
-            </Text>
-          ))}
-          {tmpData.map(item => (
-              <Line
-                  x1={x(item.label)}
-                  y1={-startYPointOfGraph + 10}
-                  x2={x(item.label)}
-                  y2={-startYPointOfGraph - graphHeight - 10}
-                  stroke={'rgba(81,81,81,0.3)'}
-                  strokeDasharray={[3, 3]}
-                  strokeWidth="0.5"
-              />
-          ))}
-          <Defs>
-            <LinearGradient id="grad" x1="0" y1="1" x2="0" y2="0">
-              <Stop offset="1" stopColor='#5BC3CE' stopOpacity="0.3" />
-              <Stop offset="0" stopColor="#FFF" stopOpacity="0" />
-            </LinearGradient>
-          </Defs>
-          {tmpData.map(item => (
-            <Circle
-              key={'c' + item.label}
-              cx={x(item.label)}
-              cy={y(item.temp) * -1}
-              r="2"
-              fill="#5BC3CE"
-            />
-          ))}
-          {tmpData.map(item => (
-              <Image
-                  x={x(item.label) - 17}
-                  y={(startYPointOfGraph + graphHeight) * -1 - 60}
-                  width={35}
-                  height={35}
-                  preserveAspectRatio="xMidYMid slice"
-                  opacity="0.6"
-                  href={require('../../../../assets/images/sun-cloud.png')}
-                  clipPath="url(#clip)"
-              />
-          ))}
-          {tmpData.map(item => (
-              <Image
-                  x={x(item.label) - 20}
-                  y={-35}
-                  width={17}
-                  height={17}
-                  preserveAspectRatio="xMidYMid slice"
-                  opacity="0.4"
-                  href={require('../../../../assets/images/rain-umbrella.png')}
-                  clipPath="url(#clip)"
-              />
-          ))}
-          {tmpData.map(item => (
-              <Text
-                  key={'ttt' + item.label}
-                  fontSize="14"
-                  x={x(item.label) + 8}
-                  y={-20}
-                  textAnchor="middle"
-                  fill="black"
-                  fontFamily="Neucha-Regular">
-                11%
-              </Text>
-          ))}
-          {tmpData.map(item => (
-            <Text
-              key={'label' + item.label}
-              fontSize="20"
-              x={x(item.label)}
-              y={(startYPointOfGraph + graphHeight) * -1 - 70}
-              textAnchor="middle"
-              fill="black"
-              fontFamily="Neucha-Regular">
-              {item.label}
-            </Text>
-          ))}
+            {item.temp + DEGREE_SIGN}
+          </Text>
+      ))}
+    </>;
+  }
+
+  generateVerticalGridLines(data, x) {
+    return <>
+      {data.map(item => (
           <Line
-            x1={0}
-            y1={-startYPointOfGraph - graphHeight}
-            x2={svgWidth}
-            y2={-startYPointOfGraph - graphHeight}
-            stroke={'rgba(81,81,81,0.3)'}
-            strokeDasharray={[3, 3]}
-            strokeWidth="0.5"
-          />
-          <Line
-              x1={50}
-              y1={-startYPointOfGraph - graphHeight/2}
-              x2={svgWidth - 50}
-              y2={-startYPointOfGraph - graphHeight/2}
+              x1={x(item.label)}
+              y1={-startYPointOfGraph + 10}
+              x2={x(item.label)}
+              y2={-startYPointOfGraph - graphHeight - 10}
               stroke={'rgba(81,81,81,0.3)'}
               strokeDasharray={[3, 3]}
               strokeWidth="0.5"
           />
-          <Line
-            x1={0}
-            y1={-startYPointOfGraph}
-            x2={svgWidth}
-            y2={-startYPointOfGraph}
-            stroke={'rgba(81,81,81,0.3)'}
-            strokeDasharray={[3, 3]}
-            strokeWidth="0.5"
-          />
-          <Text
-            fontSize="13"
-            x={20}
-            y={-startYPointOfGraph - graphHeight - 1}
-            textAnchor="middle"
-            fill="black"
-            fillOpacity={0.3}
-            fontFamily="Neucha-Regular">
-            MAX {maxValue + DEGREE_SIGN}
-          </Text>
-          <Text
-            fontSize="13"
-            x={20}
-            y={-startYPointOfGraph - 1}
-            textAnchor="middle"
-            fill="black"
-            fillOpacity={0.3}
-            fontFamily="Neucha-Regular">
-            min {minValue + DEGREE_SIGN}
-          </Text>
-
-        </G>
-      </Svg>
-    );
+      ))}
+    </>;
   }
+
+  generateFullLengthLine(y, xPadding = 0) {
+    return <Line
+        x1={xPadding}
+        y1={y}
+        x2={svgWidth - xPadding}
+        y2={y}
+        stroke={'rgba(81,81,81,0.3)'}
+        strokeDasharray={[3, 3]}
+        strokeWidth="0.5"
+    />;
+  }
+
+  generateSingleText(textValue, x, y) {
+    return <Text
+        fontSize="13"
+        x={x}
+        y={y}
+        textAnchor="start"
+        fill="black"
+        fillOpacity={0.3}
+        fontFamily="Neucha-Regular">
+      {textValue}
+    </Text>;
+  }
+
+
 }
 
-const mapStateToProps = state => {
-  return {fontLoaded: state.fontLoaded};
-};
-
-export default connect(mapStateToProps)(LearnChart);
+export default LearnChart;
