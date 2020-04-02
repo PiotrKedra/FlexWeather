1f9a64ddf19132bcf4ca4690ccd978f3bb5bb3c9
piotrkedra <piotr.k9750@gmail.com>
2020-04-02 21:40:32 +0200

    #30 new interface (looks fine)

--- src/main/weather/BasicWeatherPanel.js
+++ src/main/weather/BasicWeatherPanel.js
@@ -1,34 +1,39 @@
 import React from 'react';
-import { Image, View, StyleSheet } from 'react-native';
+import {Image, View, StyleSheet, ImageBackground} from 'react-native';
 import Text from '../components/CustomText';
 import {FORECAST_ART} from "../../resource/ImagePath";
+import Info from "./Info";
 
 class BasicWeatherPanel extends React.Component {
   render() {
     return (
       <View style={styles.mainView}>
-        <View style={{paddingLeft: '5%', borderBottomWidth: 1, borderColor: 'rgba(66,66,66,0.5)',  marginTop: 10}}>
+        <View style={{paddingLeft: '5%', marginTop: 10, borderBottomWidth: 1, borderColor: 'rgba(66,66,66,0.5)'}}>
           <Text style={{fontSize: 30, color: 'rgb(33,33,33)'}}>Friday, 3 april</Text>
           <Text style={{fontSize: 24, color: 'rgba(33,33,33,0.6)'}}>{this.props.forecastData.summary}</Text>
         </View>
-        <Image
-            style={{width: 170, height: 220, position: 'absolute', top: 60, right: 20}}
-            source={FORECAST_ART.clearDay}
-        />
-        <View style={{marginHorizontal: '5%'}}>
-          <Text style={{ fontSize: 90, color: 'rgb(33,33,33)' }}>
-            {this.props.forecastData.temperature}°
-          </Text>
-          <Text style={{ fontSize: 22, color: 'rgba(33,33,33,0.6)' }}>
-            {this.props.forecastData.temperatureMin}°/{this.props.forecastData.temperatureMax}
-          </Text>
-          <Text style={{ fontSize: 22, color: 'rgba(33,33,33,0.6)' }}>
-            Apparent {this.props.forecastData.apparentTemperatureMin}°/{this.props.forecastData.apparentTemperatureMax}
-          </Text>
-          <Text style={{ fontSize: 22, color: 'rgba(33,33,33,0.6)', marginTop: 20}}>
-            There is {this.props.forecastData.precipProbability} chance for raining.
-          </Text>
+        <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', height: 200}}>
+          <ImageBackground
+              style={{
+                flex: 1,
+                width: '100%',
+                height: '100%',
+                backgroundColor: '#0000',
+              }}
+              source={require('../../../assets/images/art/sun3.png')}
+          >
+          <View style={{marginHorizontal: '5%', alignItems: 'flex-end', flexDirection: 'row'}}>
+            <Text style={{ fontSize: 50, color: 'rgb(33,33,33)', textShadowColor: 'white', textShadowRadius: 1, textShadowOffset: {width: 2, height: 2}}}>
+              {this.props.forecastData.temperature}°
+            </Text>
+            <Text style={{ fontSize: 20, color: 'rgba(33,33,33,1)' , textShadowColor: 'white', textShadowRadius: 1, textShadowOffset: {width: 1, height: 1}}}>
+               |  {this.props.forecastData.temperatureMin}°/{this.props.forecastData.temperatureMax}°
+            </Text>
+          </View>
+          </ImageBackground>
         </View>
+
+        <Info/>
       </View>
     );
   }
