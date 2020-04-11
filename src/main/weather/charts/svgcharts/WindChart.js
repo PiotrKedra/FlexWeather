d2f3f8956e8b840637c2474389ff5ac0a75eb02b
piotrkedra <piotr.k9750@gmail.com>
2020-04-11 22:52:53 +0200

    #38 wind chart v.2

--- src/main/weather/charts/svgcharts/WindChart.js
+++ src/main/weather/charts/svgcharts/WindChart.js
@@ -1,3 +1,13 @@
+import React from "react";
+import {View} from "react-native";
+import CustomText from "../../../components/CustomText";
+
 const WindChart = () => {
 
+    return (
+        <View>
+
+       <CustomText>Filip</CustomText>
+        </View>
+    )
 };
