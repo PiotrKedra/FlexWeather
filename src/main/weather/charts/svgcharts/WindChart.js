a730585e9d57274e291479f9b0f63129a9f25522
piotrkedra <piotr.k9750@gmail.com>
2020-04-12 23:53:01 +0200

    #38 wind chart refactor

--- src/main/weather/charts/svgcharts/WindChart.js
+++ src/main/weather/charts/svgcharts/WindChart.js
@@ -1,13 +1,14 @@
 import React from "react";
 import {View} from "react-native";
+
 import CustomText from "../../../components/CustomText";
 
 const WindChart = () => {
-
     return (
         <View>
-
-       <CustomText>Filip</CustomText>
+            <CustomText>
+                Filip
+            </CustomText>
         </View>
     )
 };
