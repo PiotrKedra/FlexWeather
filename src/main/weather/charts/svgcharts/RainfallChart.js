b9e6442008ab84f27467a815e550309a21e71ce6
piotrkedra <piotr.k9750@gmail.com>
2020-04-14 22:04:15 +0200

    #26 add bars

--- src/main/weather/charts/svgcharts/RainfallChart.js
+++ src/main/weather/charts/svgcharts/RainfallChart.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {
     Svg,
     G,
-    Text
+    Text, Rect, Defs, LinearGradient, Stop, Polygon
 } from 'react-native-svg';
 import * as d3 from 'd3';
 
@@ -25,13 +25,14 @@ const RainfallChart = (props) => {
     const initialYCordOfChart = props.dimensions.initialYCordOfChart;
 
     const minValue = d3.min(data, d => d.precipIntensity);
-    const maxValue = d3.max(data, d => d.precipIntensity);
+    const maxValue = 10;//d3.max(data, d => d.precipIntensity);
     const xFunction = getFunctionX(data, svgWidth);
     const yFunction = getFunctionY(minValue, maxValue, graphHeight, initialYCordOfChart);
 
     return (
         <Svg width={svgWidth} height={svgHeight}>
             <G y={svgHeight}>
+                {getDefinitions()}
                 {getGrid(svgWidth, svgHeight, graphHeight, initialYCordOfChart, xFunction, data)}
 
                 {/* day date text */}
@@ -40,14 +41,55 @@ const RainfallChart = (props) => {
                 {/* forecast images*/}
                 {generateForecastImageForEach(data, xFunction, graphHeight, initialYCordOfChart)}
 
+                {getGradientForBars(data, xFunction, yFunction, initialYCordOfChart)}
+                {getRainfallBars(data, xFunction, yFunction, initialYCordOfChart)}
+
                 {/* data values ( text for: hour, temperature, rainfall % ) */}
                 {generateDegreeTextForEachItem(data, xFunction, yFunction)}
-                {generateTextForEachItem(data, 'precipIntensity', xFunction, 8, -20, 14)}
+                {generateTextForEachItem(data, 'humidity', xFunction, 8, -20, 14)}
                 {generateTextForEachItem(data, 'time', xFunction, 0, (initialYCordOfChart + graphHeight) * -1 - 70, 20)}
             </G>
         </Svg>
     )
 };
+
+function getRainfallBars(data, xFunction, yFunction, initialYCordOfChart){
+    return data.map(item => (
+        <Rect
+            key={item.time}
+            x={xFunction(item.time)-12.5}
+            y={yFunction(item.precipIntensity)*-1 - 2.5}
+            rx={1.5}
+            width={25}
+            height={5}
+            fill={COLORS.pathBlue}
+            opacity={0.8}
+        />
+    ))
+}
+
+function getDefinitions() {
+    return <Defs>
+        <LinearGradient id={'dupa'} x1="0" y1="1" x2="0" y2="0">
+            <Stop offset="1" stopColor={COLORS.pathBlue} stopOpacity="0.3"/>
+            <Stop offset="0" stopColor={COLORS.gradientLight} stopOpacity="0"/>
+        </LinearGradient>
+    </Defs>;
+}
+
+function getGradientForBars(data, xFunction, yFunction, initialYCordOfChart){
+    return data.map(item => (
+        <Rect
+            key={item.time}
+            x={xFunction(item.time)-12.5}
+            y={-yFunction(item.precipIntensity)}
+            rx={1.5}
+            width={25}
+            height={yFunction(item.precipIntensity) - initialYCordOfChart}
+            fill={'url(#dupa)'}
+        />
+    ))
+}
 
 function generateDegreeTextForEachItem(data, x, y) {
     return (data.map(item => (
