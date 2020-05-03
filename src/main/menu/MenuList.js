0a0cb7ab0b862c7d3d3d2855aeb6d93f555fd56d
piotrkedra <piotr.k9750@gmail.com>
2020-05-03 20:11:17 +0200

    #35 report a bug settings

--- src/main/menu/MenuList.js
+++ src/main/menu/MenuList.js
@@ -1,15 +1,14 @@
-import React, {useState} from "react";
-import {Image, TouchableOpacity, View, ScrollView, StyleSheet, Animated} from "react-native";
-import CustomText from "../components/CustomText";
+import React from "react";
+import {ScrollView} from "react-native";
 import WeatherViewSetting from "./menusettings/WeatherViewSetting";
 import HomeLocationItem from "./menusettings/HomeLocationSetting";
 import NotificationsSettings from "./menusettings/NotificatinosSettings";
 import LanguageSettings from "./menusettings/LanguageSettings";
+import ReportBugSettings from "./menusettings/ReposrtBugSettings";
 
 
 
 const MenuList = () => {
-    const [isNotification, setIsNotification] = useState(false);
 
     return (
         <ScrollView>
@@ -17,25 +16,9 @@ const MenuList = () => {
             <HomeLocationItem/>
             <NotificationsSettings/>
             <LanguageSettings/>
-            <View style={styles.menuEle}>
-                <TouchableOpacity style={styles.menuEleText} onPress={() => setIsNotification(!isNotification)}>
-                    <Image style={{height: 30, width: 30}} source={require('../../../assets/images/icons/bug.png')}/>
-                    <CustomText style={{fontSize: 25, marginHorizontal: 10}}>Report a bug</CustomText>
-                </TouchableOpacity>
-                {isNotification ? (<View style={{marginTop: 10, borderBottomWidth: 1}}><CustomText style={{fontSize: 23, marginLeft: 40, color: '#222'}}>Zabierzów</CustomText></View>): null}
-            </View>
+            <ReportBugSettings/>
         </ScrollView>
     )
 };
-
-const styles = StyleSheet.create({
-    menuEle: {
-        marginVertical: 7,
-        paddingHorizontal: 20
-    },
-    menuEleText: {
-        flexDirection: 'row'
-    }
-});
 
 export default MenuList;
