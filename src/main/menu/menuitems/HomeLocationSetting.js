0ac7a43f799439418fe6b58999e480873eab23e0
piotrkedra <piotr.k9750@gmail.com>
2020-05-03 17:30:31 +0200

    #35 home location setting refactor

--- src/main/menu/menuitems/HomeLocationSetting.js
+++ src/main/menu/menuitems/HomeLocationSetting.js
@@ -1,8 +1,7 @@
 import React, {useState} from "react";
-import {ScrollView, TouchableOpacity, StyleSheet, Image, View} from "react-native";
+import {TouchableOpacity, StyleSheet, Image, View} from "react-native";
 import CustomText from "../../components/CustomText";
 import MenuListItem from "./MenuListItem";
-import LocationSearch from "../../location/LocationSearch";
 
 const HomeLocationItem = () => {
     const [isHomeLocation, setIsHomeLocation] = useState(false);
@@ -21,28 +20,39 @@ const HomeLocationItem = () => {
 const HomeLocationSettings = () => {
 
     return (
-        <View style={{marginTop: 10, flexDirection: 'row'}}>
-            <CustomText style={{borderRadius: 10, paddingHorizontal: 10, fontSize: 23, color: '#222', flex: 5, backgroundColor: 'rgba(250,250,250,0.3)'}}>
+        <View style={styles.mainView}>
+            <CustomText style={styles.homeLocationText}>
                 Zabierzów
             </CustomText>
-            <TouchableOpacity style={{marginLeft: 10, justifySelf: 'flex-end', flex: 1}}>
-                <Image style={{width: 30, height: 30}} source={require('../../../../assets/images/icons/edit.png')}/>
+            <TouchableOpacity style={styles.editButton}>
+                <Image style={styles.editImage}
+                       source={require('../../../../assets/images/icons/edit.png')}/>
             </TouchableOpacity>
         </View>
     )
 };
 
 const styles = StyleSheet.create({
-    viewButton: {
-        backgroundColor: 'rgba(150,150,150,0.3)',
-        borderRadius: 15,
-        paddingVertical: 3,
-        paddingHorizontal: 10,
-        margin: 5
+    mainView: {
+        marginTop: 10,
+        flexDirection: 'row'
     },
-    buttonText: {
-        fontSize: 18
-    }
+    homeLocationText: {
+        flex: 5,
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        fontSize: 23,
+        color: '#222',
+        backgroundColor: 'rgba(250,250,250,0.3)'
+    },
+    editButton: {
+        flex: 1,
+        marginLeft: 10,
+    },
+    editImage: {
+        width: 30,
+        height: 30
+    },
 });
 
 export default HomeLocationItem;
