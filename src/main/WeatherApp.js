d3ebc2829502766c9d97c90c8abb8075ada25b32
piotrkedra <piotr.k9750@gmail.com>
2020-05-07 21:30:01 +0200

    #17 current geolocation position

--- src/main/WeatherApp.js
+++ src/main/WeatherApp.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Text, View } from 'react-native';
+import { Text, View, PermissionsAndroid } from 'react-native';
 
 import MainPage from './MainPage';
 import fetchRootForecast from './weather/api/ForecastApi';
+import Geolocation from '@react-native-community/geolocation';
 
 class WeatherApp extends React.Component {
   state = {
@@ -11,6 +12,44 @@ class WeatherApp extends React.Component {
   };
 
   async componentDidMount() {
+    try {
+      const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,{
+            'title': 'Location Access Required',
+            'message': 'This App needs to Access your location'
+          }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        //To Check, If Permission is granted
+        console.log('granted biach');
+        await Geolocation.getCurrentPosition(
+            (position) => {
+              console.log(position)
+            },
+            (error) => alert(error.message),
+            {enableHighAccuracy: false, timeout: 15000, maximumAge: 10000}
+        )
+      } else {
+        alert("Permission Denied");
+      }
+    } catch (err) {
+      alert("err",err);
+      console.warn(err)
+    }
+    // if(granted) {
+    //   await Geolocation.getCurrentPosition(
+    //       (position) => {
+    //         console.log(position);
+    //       },
+    //       (error) => {
+    //         // See error code charts below.
+    //         console.log('error');
+    //         console.log(error.code, error.message);
+    //       },
+    //       {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000}
+    //   );
+    // }
+    console.log('dupa');
     let rootForecast = await fetchRootForecast(50.1102653, 19.7615527);
     console.log(rootForecast.currentTimestamp);
     let entity = {
