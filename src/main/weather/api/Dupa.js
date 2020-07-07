6085369373249ec67eb820953525a2e91726b4f9
piotrkedra <piotr.k9750@gmail.com>
2020-07-07 15:21:59 +0200

    #56 add foo function

---
+++ src/main/weather/api/Dupa.js
@@ -0,0 +1,7 @@
+import dupa from 'react-native-base64';
+
+function foo(input){
+    return dupa.decode(input)
+}
+
+export default foo;
--- src/main/weather/api/ForecastApi.js
+++ src/main/weather/api/ForecastApi.js
@@ -5,7 +5,7 @@ export default fetchRootForecast;
 
 async function fetchRootForecast(lat, lng){
     try {
-        let response = await fetch('http://api.openweathermap.org/data/2.5/onecall?lat='+ lat + '&lon='+lng+'&appid=' + TOKEN + '&units=metric');
+        let response = await fetch('http://api.openweathermap.org/data/2.5/onecall?lat='+ lat + '&lon='+lng+'&appid=' + foo(a) + '&units=metric');
         let responseJson = await response.json();
         return {
             current: responseJson.current,
@@ -17,3 +17,4 @@ async function fetchRootForecast(lat, lng){
     }
 }
 
+const a = 'OWZhNjhjMDgzZjI0MjkyOGM0MWNmZWEzNDU3MDk5NTA=';
