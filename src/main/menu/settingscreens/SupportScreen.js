d15d9a9218f4f2253feae43d13ff78df3b6dcf1d
piotrkedra <piotr.k9750@gmail.com>
2020-07-11 12:55:31 +0200

    #58 finish support screen

--- src/main/menu/settingscreens/SupportScreen.js
+++ src/main/menu/settingscreens/SupportScreen.js
@@ -1,58 +1,46 @@
-import React, {useState} from 'react';
-import {Dimensions, StyleSheet, View, Text, TextInput, ScrollView, TouchableOpacity} from "react-native";
+import React from 'react';
+import {Dimensions, StyleSheet, View, Text, ScrollView, Linking, Image} from "react-native";
 
 const SupportScreen = () => {
-
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
     return (
         <ScrollView style={styles.mainView}>
+            <View style={styles.logoView}>
+                <Image style={styles.logoImage} source={require('../../../../assets/logo.png')}/>
+            </View>
             <View style={{marginVertical: 20}}>
-                <Text>
+                <Text style={[styles.text, {marginTop: 10}]}>Hi!</Text>
+                <Text style={[styles.text, {marginTop: 10}]}>Do you have any problem with the app? Or you just want to say how much you like it?</Text>
+                <Text style={{marginTop: 10}}>
                     <Text style={styles.text}>
-                        {'Hi! Do you have any problem with the app? Or you just want to say how much you like it? Write to us through the app or by sending mail to '}
+                        {'Write to me through the opinion section in '}
                     </Text>
-                    <Text style={[styles.text, {textDecorationLine: 'underline'}]}>
+                    <Text style={[styles.text, {textDecorationLine: 'underline'}]}
+                          onPress={() => redirectToStorePage()}
+                    >
+                        google play
+                    </Text>
+                    <Text style={styles.text}>{' or by sending mail to '}</Text>
+                    <Text style={[styles.text, {textDecorationLine: 'underline'}]}
+                          onPress={() => sendMail()}
+                    >
                         pkedra.studio@gmail.com
                     </Text>
                     <Text style={styles.text}>.</Text>
                 </Text>
-            </View>
-            <View>
-                <Text style={styles.inputTitleText}>your name:*</Text>
-                <TextInput
-                    multiline={true}
-                    numberOfLines={1}
-                    onChangeText={(text) => setName(text)}
-                    value={name}
-                    style={styles.textInput}
-                />
-                <Text style={styles.inputTitleText}>tell us your email if you want receive response:</Text>
-                <TextInput
-                    multiline={true}
-                    numberOfLines={1}
-                    onChangeText={(text) => setEmail(text)}
-                    value={email}
-                    style={styles.textInput}
-                />
-                <Text style={styles.inputTitleText}>message:*</Text>
-                <TextInput
-                    multiline={true}
-                    numberOfLines={10}
-                    onChangeText={(text) => setMessage(text)}
-                    value={message}
-                    style={[styles.textInput, {height: SCREEN_WIDTH/3, textAlignVertical: 'top', paddingVertical: 4}]}
-                />
-                <View style={{width: '100%', alignItems: 'flex-end', marginVertical: 20}}>
-                    <TouchableOpacity style={{backgroundColor: '#2C82C9', paddingVertical: 5, paddingHorizontal: 40, borderRadius: 5}}>
-                        <Text style={{fontFamily: 'Neucha-Regular', fontSize: 25, color: 'white'}}>send</Text>
-                    </TouchableOpacity>
-                </View>
+                <Text style={[styles.text, {marginTop: 10}]}>I will write back for sure!</Text>
             </View>
         </ScrollView>
     )
 };
+
+function sendMail() {
+    let url = 'mailto:pkedra.studio@gmail.com';
+    Linking.openURL(url);
+}
+
+function redirectToStorePage(){
+    Linking.openURL('market://details?id=myandroidappid');
+}
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
@@ -62,25 +50,18 @@ const styles = StyleSheet.create({
         backgroundColor: '#eee',
         paddingHorizontal: SCREEN_WIDTH/10
     },
+    logoView: {
+        paddingTop: 40
+    },
+    logoImage: {
+        width: 45,
+        height: 45,
+        borderRadius: 45,
+    },
     text: {
         fontFamily: 'Neucha-Regular',
-        fontSize: 22
+        fontSize: 27,
     },
-    inputTitleText: {
-        fontFamily: 'Neucha-Regular',
-        fontSize: 20,
-        color: '#444',
-        marginTop: 10,
-        marginBottom: 5,
-    },
-    textInput: {
-        fontFamily: 'Neucha-Regular',
-        fontSize: 18,
-        backgroundColor: 'white',
-        paddingHorizontal: 15,
-        paddingVertical: 0,
-        borderRadius: 5
-    }
 });
 
 export default SupportScreen;
