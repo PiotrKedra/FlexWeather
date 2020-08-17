be50b208e62654844284162385bcf1be3aafcfbb
piotrkedra <piotr.k9750@gmail.com>
2020-08-18 00:15:15 +0200

    #69 allow http method in latest android version

--- src/main/location/LocationApi.js
+++ src/main/location/LocationApi.js
@@ -1,14 +1,23 @@
-
 const getLocationDetails = async (longitude, latitude) => {
-    const url = 'http://photon.komoot.de/reverse?lon=' + longitude + '&lat=' + latitude + '&lang=en';
-    const response = await fetch(url);
-    const locationDetails = await response.json();
-    return {
-        longitude: longitude,
-        latitude: latitude,
-        city: locationDetails.features[0].properties.city,
-        country: locationDetails.features[0].properties.country,
-    };
+    try {
+        const url = 'http://photon.komoot.de/reverse?lon=' + longitude + '&lat=' + latitude + '&lang=en';
+        const response = await fetch(url);
+        const locationDetails = await response.json();
+        return {
+            longitude: longitude,
+            latitude: latitude,
+            city: locationDetails.features[0].properties.city,
+            country: locationDetails.features[0].properties.country,
+        };
+    } catch (e){
+        alert('ERROR: ' + e);
+        return {
+            longitude: longitude,
+            latitude: latitude,
+            city: 'undefined',
+            country: 'undefined',
+        };
+    }
 };
 
 export default getLocationDetails;
