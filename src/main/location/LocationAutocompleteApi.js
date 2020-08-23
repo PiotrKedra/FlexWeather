ccf9b7878cd0f7405c4d08844c4f02c1629113ec
piotrkedra <piotr.k9750@gmail.com>
2020-08-23 20:27:02 +0200

    #69 todo

--- src/main/location/LocationAutocompleteApi.js
+++ src/main/location/LocationAutocompleteApi.js
@@ -1,6 +1,7 @@
 
 
 const searchForLocationsByQuery = async  (query) => {
+    //todo parse here location so it is easy to handle them later
     const URL = 'http://photon.komoot.de/api/?lang=en&osm_tag=:city&osm_tag=:village';
     const query_param = '&q=' + query;
     const response = await fetch(URL + query_param);
