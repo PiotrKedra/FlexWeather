d770d46de3ee977c8186cdd91ef4c5bfd43cebe9
piotrkedra <piotr.k9750@gmail.com>
2020-08-30 22:57:39 +0200

    #69 removed not uses images

--- src/resource/ImagePath.js
+++ src/resource/ImagePath.js
@@ -15,21 +15,6 @@ const BACKGORUND = {
     night: require('../../assets/images/background/night-background.jpg')
 };
 
-const NEW_API = {
-    clearDay: require('../../assets/images/newapi/clear-day.png'),
-    fewClouds: require('../../assets/images/newapi/few-clouds.png'),
-    clouds: require('../../assets/images/newapi/clouds.png'),
-    brokenClouds: require('../../assets/images/newapi/broken-clouds.png'),
-    rain: require('../../assets/images/newapi/rain.png'),
-    sunRain: require('../../assets/images/newapi/sun-rain.png'),
-    thunderstorm: require('../../assets/images/newapi/thunderstorm.png'),
-    snow: require('../../assets/images/newapi/snow.png'),
-    fog: require('../../assets/images/newapi/fog.png'),
-    clearNight: require('../../assets/images/newapi/clear-night.png'),
-    fewCloudsNight: require('../../assets/images/newapi/few-clouds-night.png'),
-    nightRain: require('../../assets/images/newapi/night-rain.png'),
-};
-
 const FORECAST_ICONS2 = {
     clearDay: require('../../assets/images/forecast/clear-day.png'),
     fewClouds: require('../../assets/images/forecast/partly-cloudy-day.png'),
@@ -61,19 +46,6 @@ const FORECAST_ICONS = {
     wind: require('../../assets/images/forecast/wind.png'),
 };
 
-const FORECAST_ART = {
-    clearDay: require('../../assets/images/art/clear-day.png'),
-    clearNight: require('../../assets/images/art/clear-night.png'),
-    cloudy: require('../../assets/images/art/cloudy.png'),
-    fog: require('../../assets/images/art/fog.png'),
-    partlyCloudyDay: require('../../assets/images/art/partly-cloudy-day.png'),
-    partlyCloudyNight: require('../../assets/images/art/partly-cloudy-night.png'),
-    rain: require('../../assets/images/art/rain.png'),
-    sleet: require('../../assets/images/art/sleet.png'),
-    snow: require('../../assets/images/art/snow.png'),
-    wind: require('../../assets/images/art/wind.png'),
-};
-
-export { BACKGORUND, FORECAST_ICONS, FORECAST_ART, NEW_API, FORECAST_ICONS2};
+export { BACKGORUND, FORECAST_ICONS, FORECAST_ICONS2};
 
 export default IMAGES;
